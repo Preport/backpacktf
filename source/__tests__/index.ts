3f02c49260d908f429cdc3154cc279254bc14e38
Preport <erdemgezzz@gmail.com>
2021-05-14 02:14:59 +0300

    Update jest tests.

--- source/__tests__/index.ts
+++ source/__tests__/index.ts
@@ -10,25 +10,93 @@ test('Initialize BackpackTF connection', async (done) => {
     bp = new backpack(process.env.id, process.env.secret);
     bp.on('ready', done);
 })
+var steamID: string;
 test('Base Functions', async () => {
-    expect(await bp.getStatus()).toHaveProperty('user.id');
+    const status = await bp.getStatus();
+    expect(status.authMethod).toEqual('oauth')
+    steamID = status.user.id;
 })
 
 test('Agent Functions', async () => {
-    expect(await bp.Agent.pulse()).toHaveProperty('expire_at')
-    expect((await bp.Agent.status()).status).toEqual('active');
-    expect((await bp.Agent.stop()).status).toEqual('inactive');
-    expect((await bp.Agent.status()).status).toEqual('inactive');
+    const pulse = await bp.Agent.pulse();
+    expect(pulse).toMatchObject<backpack.Agent.PulseResponse>({
+        status: 'active',
+        client: 'got (https://github.com/sindresorhus/got)',
+        current_time: pulse.current_time
+    })
+    const status = await bp.Agent.status();
+    expect(status).toMatchObject<backpack.Agent.PulseResponse>({
+        status: 'active',
+        client: 'got (https://github.com/sindresorhus/got)',
+        current_time: status.current_time
+    });
+    const stop = await bp.Agent.stop();
+    expect(stop).toMatchObject<backpack.Agent.OnlyStatus>({
+        status: 'inactive'
+    })
+    const status2 = await bp.Agent.status();
+    expect(status2).toMatchObject<backpack.Agent.PulseResponse>({
+        status: 'inactive',
+    })
 })
 
 test('Alert Functions', async () => {
-    expect(await bp.Alerts.getAlerts()).toHaveProperty('cursor.total');
-    //Missing 3 fns Will be added after deleteAlert starts working
+    const alert = await bp.Alerts.createAlert({
+        currency: 'metal',
+        intent: 'sell',
+        item_name: 'test',
+        min: 10,
+        max: 20,
+    })
+    expect(alert).toMatchObject<typeof alert>({
+        appid: 440,
+        id: alert.id,
+        intent: 'sell',
+        item_name: 'test',
+        price: {
+            currency: 'metal',
+            min: 10,
+            max: 20
+        },
+        steamid: alert.steamid
+    })
+    await bp.Alerts.deleteAlert(alert.item_name, 'sell')
+
+    //expect it to return 404
+    try {
+        throw (await bp.Alerts.getAlert(alert.id))
+    } catch (err) {
+        expect(err.message).toEqual('Object not found')
+    }
+
+    const resp = (await bp.Alerts.getAlerts()).results
+    expect(resp).not.toContain<backpack.Alerts.Alert>({
+        appid: 440,
+        id: alert.id,
+        intent: 'sell',
+        item_name: 'test',
+        steamid: steamID,
+        price: {
+            currency: 'metal',
+            min: 10,
+            max: 20
+        }
+    })
 })
 
 test('Notification Functions', async () => {
-    expect(await bp.Notifications.getNotifications()).toHaveProperty('cursor.total');
-    //Missing 4 fns Will be added after deleteNotification starts working
+
+    const resp = await bp.Notifications.getNotifications()
+    expect(resp).toHaveProperty('cursor.limit')
+
+    if (resp.results.length) {
+        const notification = await bp.Notifications.getNotification(resp.results[0].id);
+        expect(notification).toHaveProperty('bundle');
+    }
+
+    expect(typeof await bp.Notifications.unreadNotifications()).toEqual('object');
+
+    expect(await bp.Notifications.markNotifications()).toHaveProperty('modified')
 })
 
 test('Inventory Functions', async () => {
