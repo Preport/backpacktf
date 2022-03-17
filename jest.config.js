815e4dfb4a36d4b607dcc790fc487c4d7efd86fc
Preport <erdemgezzz@gmail.com>
2022-03-17 15:32:31 +0300

    jest config, lockfile v2, rollback got major version

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)']
+    testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    verbose: true,
 };
